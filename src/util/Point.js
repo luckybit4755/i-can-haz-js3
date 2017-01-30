91e11c881510d9c82a3e168116c0014b4aa80ffb
luckybit4755 <luckybit4755@gmail.com>
2017-01-30 13:14:11 -0500

    various pinhole optimizations

--- src/util/Point.js
+++ src/util/Point.js
@@ -1,110 +1,108 @@
-const Point = function(x,y,z) {
-	const self = this;
+const Point = function() {
+	this.value = [ [0], [0], [0], [0] ];
+};
 
-	self.init = function(x,y,z) {
-		self.initXYZ( x?x:0, y?y:0, z?z:0 );
-	};
+Point.prototype = {
+	init: function(x,y,z) {
+		this.initXYZ( x?x:0, y?y:0, z?z:0 );
+	}
 
-	self.initXYZ = function( x, y, z ) {
-		self.value = [ [x], [y], [z], [0] ];
-		return self;
-	};
+	, initXYZ: function( x, y, z ) {
+		this.value = [ [x], [y], [z], [0] ];
+		return this;
+	}
 
-	self.initPoint = function( other ) {
-		return self.initValue( other.value );
-	};
+	, initPoint: function( other ) {
+		return this.initValue( other.value );
+	}
 
-	self.initValue = function( value ) {
-		return self.initXYZ( value[ 0 ][ 0 ], value[ 1 ][ 0 ], value[ 2 ][ 0 ] );
-	};
+	, initValue: function( value ) {
+		return this.initXYZ( value[ 0 ][ 0 ], value[ 1 ][ 0 ], value[ 2 ][ 0 ] );
+	}
 
-	self.initArray = function( value ) {
-		return self.initXYZ( value[ 0 ], value[ 1 ], value[ 2 ] );
-	};
+	, initArray: function( value ) {
+		return this.initXYZ( value[ 0 ], value[ 1 ], value[ 2 ] );
+	}
 
-	self.toXYZ = function() {
+	, toXYZ: function() {
 		return {
-			  x : self.value[ 0 ][ 0 ]
-			, y : self.value[ 1 ][ 0 ]
-			, z : self.value[ 2 ][ 0 ]
+			  x : this.value[ 0 ][ 0 ]
+			, y : this.value[ 1 ][ 0 ]
+			, z : this.value[ 2 ][ 0 ]
 		};
 	}
 
-	self.copy = function() {
-		return ( new Point() ).initPoint( self );
-	};
+	, copy: function() {
+		return ( new Point() ).initPoint( this );
+	}
 
-	self.x = function() { return self.value[ 0 ][ 0 ]; }
-	self.y = function() { return self.value[ 1 ][ 0 ]; }
-	self.z = function() { return self.value[ 2 ][ 0 ]; }
+	, x: function() { return this.value[ 0 ][ 0 ] }
+	, y: function() { return this.value[ 1 ][ 0 ] }
+	, z: function() { return this.value[ 2 ][ 0 ] }
 
-	self.minus = function( other ) {
-		self.value[ 0 ][ 0 ] -= other.value[ 0 ][ 0 ];
-		self.value[ 1 ][ 0 ] -= other.value[ 1 ][ 0 ];
-		self.value[ 2 ][ 0 ] -= other.value[ 2 ][ 0 ];
-		return self;
-	};
+	, minus: function( other ) {
+		this.value[ 0 ][ 0 ] -= other.value[ 0 ][ 0 ];
+		this.value[ 1 ][ 0 ] -= other.value[ 1 ][ 0 ];
+		this.value[ 2 ][ 0 ] -= other.value[ 2 ][ 0 ];
+		return this;
+	}
 
-	self.add = function( other ) {
-		self.value[ 0 ][ 0 ] += other.value[ 0 ][ 0 ];
-		self.value[ 1 ][ 0 ] += other.value[ 1 ][ 0 ];
-		self.value[ 2 ][ 0 ] += other.value[ 2 ][ 0 ];
-		return self;
-	};
+	, add: function( other ) {
+		this.value[ 0 ][ 0 ] += other.value[ 0 ][ 0 ];
+		this.value[ 1 ][ 0 ] += other.value[ 1 ][ 0 ];
+		this.value[ 2 ][ 0 ] += other.value[ 2 ][ 0 ];
+		return this;
+	}
 
-	self.normalize = function() {
-		var size2 = self.size2();
+	, normalize: function() {
+		var size2 = this.size2();
 		if ( 0 == size2 ) return;
 		size2 = Math.sqrt( size2 );
 
-		self.value[ 0 ][ 0 ] /= size2;
-		self.value[ 1 ][ 0 ] /= size2;
-		self.value[ 2 ][ 0 ] /= size2;
-		return self;
-	};
+		this.value[ 0 ][ 0 ] /= size2;
+		this.value[ 1 ][ 0 ] /= size2;
+		this.value[ 2 ][ 0 ] /= size2;
+		return this;
+	}
 
-	self.size = function() {
-		return Math.sqrt( self.size2() );
-	};
+	, size: function() {
+		return Math.sqrt( this.size2() );
+	}
 
-	self.size2 = function() {
-		return self.dot( self );
-	};
+	, size2: function() {
+		return this.dot( this );
+	}
 
-	self.dot = function( other ) {
+	, dot: function( other ) {
 		return ( 0 +
-			+ self.value[ 0 ][ 0 ] * other.value[ 0 ][ 0 ]
-			+ self.value[ 1 ][ 0 ] * other.value[ 1 ][ 0 ]
-			+ self.value[ 2 ][ 0 ] * other.value[ 2 ][ 0 ]
+			+ this.value[ 0 ][ 0 ] * other.value[ 0 ][ 0 ]
+			+ this.value[ 1 ][ 0 ] * other.value[ 1 ][ 0 ]
+			+ this.value[ 2 ][ 0 ] * other.value[ 2 ][ 0 ]
 		);
-	};
+	}
 
-	self.cross = function( b, c ) {
-		return self.initXYZ(
+	, cross: function( b, c ) {
+		return this.initXYZ(
 			  b.value[ 1 ][ 0 ] * c.value[ 2 ][ 0 ] - b.value[ 2 ][ 0 ] * c.value[ 1 ][ 0 ] //b.y * c.z - b.z - c.y
 			, b.value[ 2 ][ 0 ] * c.value[ 0 ][ 0 ] - b.value[ 0 ][ 0 ] * c.value[ 2 ][ 0 ] //b.z * c.x - b.x - c.z
 			, b.value[ 0 ][ 0 ] * c.value[ 1 ][ 0 ] - b.value[ 1 ][ 0 ] * c.value[ 0 ][ 0 ] //b.x * c.y - b.y - c.x
 		);
-	};
+	}
 
-	self.toString = function() {
-		return JSON.stringify( self.toXYZ() );
-	};
+	, toString: function() {
+		return JSON.stringify( this.toXYZ() );
+	}
 
-	self.scale = function( scalar ) {
-		self.value[ 0 ][ 0 ] *= scalar;
-		self.value[ 1 ][ 0 ] *= scalar;
-		self.value[ 2 ][ 0 ] *= scalar;
+	, scale: function( scalar ) {
+		this.value[ 0 ][ 0 ] *= scalar;
+		this.value[ 1 ][ 0 ] *= scalar;
+		this.value[ 2 ][ 0 ] *= scalar;
 		return this;
-	};
+	}
 
-	self.tween = function( a, b, t ) {
-		return self.initPoint( a ).minus( b ).scale( t ).add( b );
-	};
-
-	///
-
-	self.init( x, y, z );
+	, tween: function( a, b, t ) {
+		return this.initPoint( a ).minus( b ).scale( t ).add( b );
+	}
 };
 
 try {
--- src/weak/TriangularB6.js
+++ src/weak/TriangularB6.js
@@ -1,33 +1,23 @@
-/* this doesn't really belong here, but the web interface for github is so ridiculous and I didn't want to fool with the CLI, here it is for now... */
-
-/*from https://sites.google.com/site/proyectosroboticos/bresenham-6d*/
-const bresenham6d_min = function(a, f, o, i, r, s, t, b, h, n, M, c, e) {
-    var l, u, v, d, g, j, m, k, p, q, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, S, T, U, V, W;
-    if (l = u = v = d = g = j = m = k = p = q = w = x = y = z = A = B = C = D = E = F = G = H = I = J = K = L = N = O = P = Q = R = S = T = U = V = W = 0, e || (e = function(a, f, o, i, r, s) {}), K = a, L = f, N = o, O = i, P = r, Q = s, e(K, L, N, O, P, Q), l = t - a, u = b - f, v = h - o, d = n - i, g = M - r, j = c - s, y = 0 > l ? -1 : 1, z = 0 > u ? -1 : 1, A = 0 > v ? -1 : 1, B = 0 > d ? -1 : 1, C = 0 > g ? -1 : 1, D = 0 > j ? -1 : 1, m = Math.abs(l), k = Math.abs(u), p = Math.abs(v), q = Math.abs(d), w = Math.abs(g), x = Math.abs(j), E = 2 * m, F = 2 * k, G = 2 * p, H = 2 * q, I = 2 * w, J = 2 * x, m >= k && m >= p && m >= q && m >= w && m >= x)
-        for (R = F - m, S = G - m, T = H - m, U = I - m, V = J - m, W = 1; m > W; W++) R > 0 && (L += z, R -= E), S > 0 && (N += A, S -= E), T > 0 && (O += B, T -= E), U > 0 && (P += C, U -= E), V > 0 && (Q += D, V -= E), R += F, S += G, T += H, U += I, V += J, K += y, e(K, L, N, O, P, Q);
-    if (k > m && k >= p && k >= q && k >= w && k >= x)
-        for (R = E - k, S = G - k, T = H - k, U = I - k, V = J - k, W = 1; k > W; W++) R > 0 && (K += y, R -= F), S > 0 && (N += A, S -= F), T > 0 && (O += B, T -= F), U > 0 && (P += C, U -= F), V > 0 && (Q += D, V -= F), R += E, S += G, T += H, U += I, V += J, L += z, e(K, L, N, O, P, Q);
-    if (p > m && p > k && p >= q && p >= w && p >= x)
-        for (R = F - p, S = E - p, T = H - p, U = I - p, V = J - p, W = 1; p > W; W++) R > 0 && (L += z, R -= G), S > 0 && (K += y, S -= G), T > 0 && (O += B, T -= G), U > 0 && (P += C, U -= G), V > 0 && (Q += D, V -= G), R += F, S += E, T += H, U += I, V += J, N += A, e(K, L, N, O, P, Q);
-    if (q > m && q > k && q > p && q >= w && q >= x)
-        for (R = E - q, S = F - q, T = G - q, U = I - q, V = J - q, W = 1; q > W; W++) R > 0 && (K += y, R -= H), S > 0 && (L += z, S -= H), T > 0 && (N += A, T -= H), U > 0 && (P += C, U -= H), V > 0 && (Q += D, V -= H), R += E, S += F, T += G, U += I, V += J, O += B, e(K, L, N, O, P, Q);
-    if (w > m && w > k && w > p && w > q && w >= x)
-        for (R = E - w, S = F - w, T = G - w, U = H - w, V = J - w, W = 1; w > W; W++) R > 0 && (K += y, R -= I), S > 0 && (L += z, S -= I), T > 0 && (N += A, T -= I), U > 0 && (O += B, U -= I), V > 0 && (Q += D, V -= I), R += E, S += F, T += G, U += H, V += J, P += C, e(K, L, N, O, P, Q);
-    if (x > m && x > k && x > p && x > q && x > w)
-        for (R = E - x, S = F - x, T = G - x, U = H - x, V = I - x, W = 1; x > W; W++) R > 0 && (K += y, R -= J), S > 0 && (L += z, S -= J), T > 0 && (N += A, T -= J), U > 0 && (O += B, U -= J), V > 0 && (P += C, V -= J), R += E, S += F, T += G, U += H, V += I, Q += D, e(K, L, N, O, P, Q)
-};
-
-const bresenham6d = function(Old1, Old2, Old3, Old4, Old5, Old6, New1, New2, New3, New4, New5, New6, callback) {
-    bresenham6d_min(Old1, Old2, Old3, Old4, Old5, Old6, New1, New2, New3, New4, New5, New6, callback);
-};
-
+/**
+ *
+ * Simple rendering engine for triangles that use a zbuffer, 6d implementation
+ * of the bresenham line drawing algorithm and a simplistic scanline triangle
+ * rendering mechanism similar to my work with Ghia
+ * 
+ * It supports primitive texturing and shading.
+ *
+ */
 const TriangularB6 = function(canvas) {
     var self = this;
     self.zFar = 3344;
+
     self.init = function(canvas) {
 		self.fillStyle = 'black';
         self.canvas = canvas || document.getElementsByTagName('canvas')[0];
         self.context = self.canvas.getContext('2d');
+
+		self.w = parseInt( self.canvas.width );
+		self.h = parseInt( self.canvas.height );
 
         /* create the zbuffer and scan line buffers */
 
@@ -35,12 +25,22 @@ const TriangularB6 = function(canvas) {
         self.left = [];
         self.right = [];
 
-        for (var i = 0; i < self.canvas.height; i++) {
+		// avoid object creation as much as possible
+        self.leftPoint = [];
+        self.rightPoint = [];
+
+        for (var y = 0; y < self.h; y++) {
             self.left.push(false);
             self.right.push(false);
-            self.zbuffer.push([]);
-            for (var j = 0; j < self.canvas.width; j++) {
-                self.zbuffer[i][j] = self.zFar;
+
+			self.leftPoint.push( self.newPoint() );
+			self.rightPoint.push( self.newPoint() );
+
+			var zbufferRow = [];
+            self.zbuffer.push( zbufferRow );
+
+            for (var x = 0; x < self.w; x++ ) {
+				zbufferRow[ x ] = self.zFar;
             }
         }
     };
@@ -52,9 +52,9 @@ const TriangularB6 = function(canvas) {
     };
 
     self.clearPixels = function() {
-        self.context.fillStyle = self.fillStyle;
-        self.context.fillRect(0, 0, self.canvas.width, self.canvas.height);
-        self.imageData = self.context.getImageData(0, 0, self.canvas.width, self.canvas.height);
+		self.context.fillStyle = self.fillStyle;
+		self.context.fillRect(0, 0, self.w, self.h);
+		self.imageData = self.context.getImageData(0, 0, self.w, self.h);
     };
 
     self.flush = function() {
@@ -62,17 +62,21 @@ const TriangularB6 = function(canvas) {
     };
 
     self.clearZbuffer = function() {
-        for (var i = 0; i < self.canvas.height; i++) {
-            for (var j = 0; j < self.canvas.width; j++) {
-                self.zbuffer[i][j] = self.zFar;
+		var h = self.h;
+		var w = self.w;
+		var z = self.zFar;
+		var b = self.zbuffer;
+        for (var y = 0; y < h ; y++) {
+			var row = b[ y ];
+            for (var x = 0; x < w ; x++) {
+               	row[ x ] = z;
             }
         }
     };
 
     self.clearScanlines = function() {
-        for (var i = 0; i < self.canvas.height; i++) {
-            self.left[i] = self.right[i] = false;
-        }
+		self.left.fill( false );
+		self.right.fill( false );
     };
     
     self.setTexture = function( pixels, w, h ) {
@@ -81,83 +85,15 @@ const TriangularB6 = function(canvas) {
         self.textureH = h;
     };
 
-    self.zput = function(x, y, z, r, g, b) {
-        if ( y < 0 ) y  = 0;
-        if ( y >= self.canvas.height ) y = self.canvas.height - 1;
-        if ( x < 0 ) x  = 0;
-        if ( x >= self.canvas.width ) x = self.canvas.width - 1;
-        
-        x = Math.floor(x);
-        y = Math.floor(y);
-        
-        if (z < self.zbuffer[y][x]) {
-            self.zbuffer[y][x] = z;
-            var index = 4 * (x + y * self.canvas.width);
-            var zCue = 64 + 192 - 192 * z / self.canvas.width;
-            
-            /* texture hack for the win! */
-            if ( -1 == b && self.texture ) {
-                   var s = Math.floor( self.textureW * r / 255 );
-                   var t = Math.floor( self.textureH * g / 255 );
-                   while ( s < 0 ) s += self.textureW;
-                   while ( t < 0 ) t += self.textureH;
-                   while ( s >= self.textureW ) s -= self.textureW;
-                   while ( t >= self.textureH ) t -= self.textureH;
-                   var tIndex = 4 * ( s + t * self.textureW );
-                    r = self.texture[ tIndex++ ];
-                    g = self.texture[ tIndex++ ];
-                    b = self.texture[ tIndex++ ];
-            }
-
-			if ( self.normal ) {
-				var q = 0.1 + self.normal.z * 0.9;
-				r = Math.floor( r * q );
-				g = Math.floor( g * q );
-				b = Math.floor( b * q );
-			}
-
-            self.imageData.data[index++] = r;
-            self.imageData.data[index++] = g;
-            self.imageData.data[index++] = b;
-            self.imageData.data[index++] = 255;//zCue;
-        }
-    };
-
-    self.makePoint = function(x, y, z, r, g, b) {
-        return {
-            x: x,
-            y: y,
-            z: z,
-            r: r,
-            g: g,
-            b: b
-        };
-    };
-
-    self.scanner = function(x, y, z, r, g, b) {
-        if (y < 0 || y >= self.canvas.height) return;
-
-        /* for fun */
-		if ( self.outline ) {
-        	self.zput(x, y, z -0.0001, x, y, z);
-		}
-
-        if (!self.left[y] || x <= self.left[y].x) {
-            self.left[y] = self.makePoint(x-1, y, z, r, g, b);
-        }
-        if (!self.right[y] || x >= self.right[y].x) {
-            self.right[y] = self.makePoint(x+1, y, z, r, g, b);
-        }
-    };
-
     self.triangleDraw = function(pt1, pt2, pt3, normal ) {
         self.clearScanlines();
 		self.normal = normal;
 
         var minY = Math.min(pt1.y, Math.min(pt2.y, pt3.y));
         var maxY = Math.max(pt1.y, Math.max(pt2.y, pt3.y)) + 1;
+
         if (minY < 0) minY = 0;
-        if (maxY >= self.canvas.height) maxY = self.canvas.height;
+        if (maxY >= self.h) maxY = self.h;
 
         /* scan the 3 sides of the triangle */
 
@@ -191,6 +127,73 @@ const TriangularB6 = function(canvas) {
         };
     };
 
+    self.scanner = function(x, y, z, r, g, b) {
+        if ( y < 0 || y >= self.h ) return;
+
+        /* for fun */
+		if ( self.outline ) {
+        	self.zput(x, y, z -0.0001, x, y, z);
+		}
+
+        if (!self.left[y] || x <= self.left[y].x) {
+            self.left[y] = self.setPoint( self.leftPoint[ y ], x-1, y, z, r, g, b);
+        }
+        if (!self.right[y] || x >= self.right[y].x) {
+            self.right[y] = self.setPoint( self.rightPoint[ y ], x+1, y, z, r, g, b);
+        }
+    };
+
+    self.zput = function( x, y, z, r, g, b ) {
+        if ( y < 0 || x < 0 || y >= self.h || x >= self.w ) return; 
+	  
+		x = Math.floor( x );
+		y = Math.floor( y );
+					
+        if ( z > self.zbuffer[ y ][ x ]) return;
+
+		self.zbuffer[ y ][ x ] = z;
+		var index = 4 * ( x + y * self.w );
+		
+		/* texture hack for the win! */
+		if ( -1 == b && self.texture ) {
+			var s = Math.floor( r * self.textureW / 256);	
+			var t = Math.floor( g * self.textureH / 256 );	
+			if ( s < 0 ) s+= self.textureW;
+			if ( t < 0 ) t+= self.textureH;
+
+			var tIndex = 4 * ( s + t * self.textureW );
+			r = self.texture[ tIndex + 0 ];
+			g = self.texture[ tIndex + 1 ];
+			b = self.texture[ tIndex + 2 ];
+		}
+
+		if ( self.normal ) {
+			var q = 0.1 + self.normal.z * 0.9;
+			r *= q;
+			g *= q;
+			b *= q;
+		}
+
+		self.imageData.data[index + 0 ] = r;
+		self.imageData.data[index + 1 ] = g;
+		self.imageData.data[index + 2 ] = b;
+		self.imageData.data[index + 3 ] = 255;
+    };
+	
+    self.newPoint = function() {
+		return self.makePoint(0,0,0, 0,0,0);
+    };
+
+    self.makePoint = function(x, y, z, r, g, b) {
+        return { x: x, y: y, z: z, r: r, g: g, b: b };
+    };
+
+    self.setPoint = function(point, x, y, z, r, g, b) {
+		point.x = x; point.y = y; point.z = z; point.r = r; point.g = g; point.b = b;
+		return point;
+    };
+
     /*******/
+
     self.init(canvas);
 };
